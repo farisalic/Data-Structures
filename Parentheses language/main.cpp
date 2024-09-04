192075fac8b98261eb5c209c64548010b6fbad2a
farisalic <farisalic02@gmail.com>
2024-09-04 19:16:51 +0200

    Add files via upload

---
+++ Parentheses language/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+bool boltic(const std::string& temp){
+std::stack<char> zagrade;
+
+for (char x : temp)
+{
+  switch (x)
+  {
+  case '{':
+    if (zagrade.top() != '[' || zagrade.top() != '(' || zagrade.top() != '<') zagrade.push(x);
+    break;
+
+  case '[':
+    if (zagrade.top() != '(' || zagrade.top() != '<') zagrade.push(x);
+    break;
+
+  case '(':
+    if (zagrade.top() != '<') zagrade.push(x);
+    break;
+
+  case '<':
+    zagrade.push(x);
+    break;
+
+  case '>':
+    if (zagrade.empty() || zagrade.top() != '<') return false;
+    else zagrade.pop();
+    break;
+
+  case ')':
+    if (zagrade.empty() || zagrade.top() != '(') return false;
+    else zagrade.pop();
+    break;
+
+  case ']':
+    if (zagrade.empty() || zagrade.top() != '[') return false;
+    else zagrade.pop();
+    break;
+
+  case '}':
+    if (zagrade.empty() || zagrade.top() != '{') return false;
+    else zagrade.pop();
+    break;
+
+  default:
+    return false;
+  }
+}
+};
+
+int main(int argc, char *argv[])
+{
+  std::string temp;
+  std::cout << "BOLTIC" << std::endl;
+
+  while(true){
+    std::cout << "Unesite vas izraz: " << std::endl;
+    std::getline(std::cin, temp);
+
+    if(boltic(temp) == true) std::cout << "dobar" << std::endl;
+    else std::cout << "pogresan" << std::endl;
+}
+  return 0;
+};
+
+/*To fix: povremeni seg fault u slučaju nedozvoljene kombinacije zagrada na ulazu*/
